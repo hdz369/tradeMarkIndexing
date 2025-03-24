c083d1c0935b8390ea4a85b4752ef28fa9ff9953
huangdezhao <dzhuang369@gmail.com>
2025-03-24 18:28:56 +0800

    put model loading to flash stage and separate cpu/gpu run

---
+++ start_docker_gpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export WORKSPACE=$HOME/tradeMarkIndexing
+
+# export DATA=$HOME/huangdezhao/data
+
+docker_image="huangdezhao/trademark:latest"
+
+# docker run -it -w $WORKSPACE -v $(PWD):$WORKSPACE -p 8080:8080 $docker_image     # for cpu usage
+docker run -it --gpus all -w $WORKSPACE -v $(PWD):$WORKSPACE -p 8080:8080 $docker_image   # for gpu usage
